3488047817d668714dee897bc717123ed826c689
Bryan3458 <bryan.a.martinez01@utrgv.edu>
2025-02-01 16:03:30 +0000

    Couldnt figure out how to round to the hundretth place

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,21 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        float checking = 2175.37f;
 
+        checking -= 302.50;
+
+        checking += 50.03;
+
+        checking /= 2;
+
+        checking += 20.00;
+
+        checking -= 1;
+
+        checking *= 2;
+
+        checking += 1;
+
+        System.out.print(checking); 
+    }
+}
