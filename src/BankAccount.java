fe52e810cc88b94b1113d9d5b0b929a92f01d8e0
Bryan3458 <bryan.a.martinez01@utrgv.edu>
2025-02-01 17:03:05 +0000

    Nvm just rewatched the lecture recording got the answer to the hundreth decimal

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,6 @@
 public class BankAccount {
     public static void main(String[] args) {
-        float checking = 2175.37f;
+        double checking = 2175.37;
 
         checking -= 302.50;
 
@@ -16,6 +16,6 @@ public class BankAccount {
 
         checking += 1;
 
-        System.out.print(checking); 
+        System.out.printf("%.2f", checking); 
     }
 }
